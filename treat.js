2d4dc73749a9ccd3f078458681c8f38291df583c
leamonsenego <116014403+leamonsenego@users.noreply.github.com>
2023-08-01 20:17:15 +0200

    Create treat.js

---
+++ treat.js
@@ -0,0 +1,34 @@
+class Treat {
+  static treats = []
+  constructor(gameScreen){
+    this.gameScreen = gameScreen;
+    this.left = Math.floor(Math.random() * 300 + 70);
+    this.top = 0;
+    this.width = 100;
+    this.height = 100;
+    this.element = document.createElement("img");
+
+    this.element.src="./images/dogstreats.jpeg";
+    this.element.style.position = "absolute";
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+
+    this.gameScreen.appendChild(this.element);
+
+    Treat.treats = [...Treat.treats, this]
+  }
+
+  updatePosition(){
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+
+  move(){
+    // Move the object down and update its position on the screen
+    this.top = this.top + 3;
+    this.updatePosition();
+  }
+
+}
